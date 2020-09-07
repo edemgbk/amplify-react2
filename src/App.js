a690ff32fc38bf212ae5ecf1d9a499f135df4e55
edem kofi gbeku <edemgbk@edems-MacBook-Pro.local>
2020-09-07 13:37:35 +0000

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import awsconfig from './aws-exports';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
